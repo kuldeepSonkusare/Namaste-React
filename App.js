4668b21bf029ba4c94ee639e8991e754108d01d7
kuldeepSonkusare <sonkusarekuldeep@gmail.com>
2024-12-17 14:25:15 +0530

    fourth ep

--- App.js
+++ App.js
@@ -1,22 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1", { id: "heading"}, "hello from react");
-
-const Title =() => (
-    <h1 className="head">
-        Namaste react using JSX!
-    </h1>
-)
-
-const HeadingComponent = () => (
-    <div id="container">
-
-        <Title/>
-        <h1 className="heading">Namaste react functional component</h1>
-
-    </div>
-)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
